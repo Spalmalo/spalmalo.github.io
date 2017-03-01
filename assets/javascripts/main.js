db8e460bb7454d7d869c521f211638c4e5b4ef6b
Velichko Konstantin <zoxon.box@gmail.com>
2017-03-01 14:31:01 +0600

    Initial commit

---
+++ assets/javascripts/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+svg4everybody();
+
+$(function() {
+	FastClick.attach(document.body);
+});
+
+$('.table').basictable({ baseClass: 'table' });
+
+// Paralax background
+(function(){
+	var $window = $(window);
+	$('[data-paralax-type="background"]').each(function(){
+		var $bgobj = $(this);
+
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('paralax-speed'));
+			var coords = '50% '+ yPos + 'px';
+			$bgobj.css({
+				backgroundPosition: coords
+			});
+		});
+	});
+})();
+
+
+// Modules
+//= require source/modules/**/!(_)*.js
+
